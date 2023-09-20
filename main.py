218ce3b73e11e57e906106a1d388376a283d85a5
nightconcept <dark@nightconcept.net>
2023-09-20 08:47:49 -0700

    Working with old bad physics
    More conversions over to a generic window draw function

--- main.py
+++ main.py
@@ -21,13 +21,12 @@ CONTROL_P2_UP = pygame.K_UP
 CONTROL_P2_DOWN = pygame.K_DOWN
 
 ## Events
-class GameEvents(Enum):
-    P1_SCORED = pygame.USEREVENT + 1
-    P2_SCORED = pygame.USEREVENT + 2
+P1_SCORED = pygame.USEREVENT + 1
+P2_SCORED = pygame.USEREVENT + 2
 
 ## Fonts
 SCORE_FONT = pygame.font.Font(os.path.join('assets', 'bit5x3.ttf'), 50)
-START_FONT = pygame.font.Font(os.path.join('assets', 'bit5x3.ttf'), 20)
+START_FONT = pygame.font.Font(os.path.join('assets', 'bit5x3.ttf'), 40)
 
 ## Game Specific
 PLAYER_VEL = 30
@@ -44,16 +43,21 @@ P1_STARTING_X = 100
 P1_STARTING_Y = 300
 P2_STARTING_X = 700
 P2_STARTING_Y = 300
+SCORE_DELAY_TIME = 2000
 
 ## Screen and settings
 FPS = 60
 VOLUME = 0.3
 WINDOW_WIDTH, WINDOW_HEIGHT = 858, 525 # Based on actual pong resolution
+TEXT_ANTIALIAS_TRUE = 1
 
-## Sounds
+## Sounds and config
 BALL_HIT_SOUND_1 = pygame.mixer.Sound(os.path.join('assets', 'ping_pong_8bit_plop.ogg'))
 BALL_HIT_SOUND_2 = pygame.mixer.Sound(os.path.join('assets', 'ping_pong_8bit_beeep.ogg'))
 SCORE_SOUND = pygame.mixer.Sound(os.path.join('assets', 'ping_pong_8bit_peeeeeep.ogg'))
+BALL_HIT_SOUND_1.set_volume(VOLUME)
+BALL_HIT_SOUND_2.set_volume(VOLUME)
+SCORE_SOUND.set_volume(VOLUME)
 
 ## States
 class GameStates(Enum):
@@ -63,46 +67,85 @@ class GameStates(Enum):
 # Classes
 ## Window
 class Window:
+    """Window class that is the pygame display and manages all the items that need to be drawn on screen"""
     def __init__(self, width, height, caption):
         self.win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.draw_items = []
         pygame.display.set_caption(caption)
 
-    def draw():
+    # TODO: Before using this class type in another game and draw order may be important, this draw order
+    # may become relevant at some point
+    def draw(self):
         self.win.fill(BLACK)
+        for item in self.draw_items:
+            item.draw()
         pygame.display.update()
 
+    def add_draw_item(self, item):
+        self.draw_items.append(item)
+
+    def get_game_state(self):
+        return self.game_state
+
+    def set_game_state(self, game_state):
+        self.game_state = game_state
+
+    def show_text(self, text):
+        draw_text = START_FONT.render(text, TEXT_ANTIALIAS_TRUE, WHITE)
+        self.win.blit(draw_text, (WINDOW_WIDTH//2 - draw_text.get_width()//2, (WINDOW_HEIGHT//4)*3 - draw_text.get_height()//2))
+        pygame.display.update()
+
+    def get_surface(self):
+        return self.win
 
 ## Paddle
 class Paddle:
-    def __init__(x, y, width, height, player):
+    """Paddle class that represents a player and score element in pong"""
+    def __init__(self, window, x, y, width, height, player):
         self.rect = pygame.Rect(x, y, width, height)
         self.player = player
         self.score = 0
+        self.window = window
     
+    # TODO: Improve the way a bind is handled instead of hard coding a player mapping here
     def handle_movement(self, keys_pressed):
         if self.player == 1:
             if keys_pressed[CONTROL_P1_UP] and self.rect.y - PLAYER_VEL > PLAYER_AREA_MARGIN: # UP
-                player1.y -= PLAYER_VEL
-            if keys_pressed[CONTROL_P1_DOWN] and selft.rect.y + PLAYER_VEL + self.rect.height < WINDOW_HEIGHT - PLAYER_AREA_MARGIN: # DOWN
-                player1.y += PLAYER_VEL
+                self.rect.y -= PLAYER_VEL
+            if keys_pressed[CONTROL_P1_DOWN] and self.rect.y + PLAYER_VEL + self.rect.height < WINDOW_HEIGHT - PLAYER_AREA_MARGIN: # DOWN
+                self.rect.y += PLAYER_VEL
         else:
             if keys_pressed[CONTROL_P2_UP] and self.rect.y - PLAYER_VEL > PLAYER_AREA_MARGIN: # UP
                 self.rect.y -= PLAYER_VEL
             if keys_pressed[CONTROL_P2_DOWN] and self.rect.y + PLAYER_VEL + self.rect.height < WINDOW_HEIGHT - PLAYER_AREA_MARGIN: # DOWN
                 self.rect.y += PLAYER_VEL
     
-    def get_rect():
+    def scored(self, points = 1):
+        self.score += points
+
+    def get_score(self):
+        return self.score
+
+    def get_rect(self):
         return self.rect
+
+    def draw(self):
+        pygame.draw.rect(self.window, WHITE, self.rect)
+        score = SCORE_FONT.render(str(self.score), TEXT_ANTIALIAS_TRUE, WHITE)
+        if self.player == 1:
+            score_coordinates = (WINDOW_WIDTH//4 - score.get_width()//2, 10) 
+        else:
+            score_coordinates = ((WINDOW_WIDTH//4)*3 - score.get_width()//2, 10)
+        self.window.blit(score, score_coordinates)
 
 ## Ball
 class Ball:
     """The ball that is hit by the paddles in pong"""
-    def __init__(self):
-        self.rect = pygame.draw.circle(WINDOW, WHITE, (-2 * BALL_RADIUS, -2 * BALL_RADIUS), BALL_RADIUS)
+    def __init__(self, window):
+        self.window = window
 
-    def spawn(self, x, y):
-        self.rect.x = x
-        self.rect.y = y
+    def spawn(self, x = WINDOW_WIDTH//2, y = WINDOW_HEIGHT//2):
+        self.rect = pygame.draw.circle(self.window, WHITE, (x, y), BALL_RADIUS)
         self.x_vel = random.randint(BALL_START_VEL//2, BALL_START_VEL)
         if random.randint(0, 1) == 1:
             self.x_vel *= -1
@@ -115,13 +158,13 @@ class Ball:
         self.rect.y += self.y_vel
         for paddle in paddles:
             if self.rect.colliderect(paddle):
-                # collision physics
-                # intersect_y = (player1.centery + PADDLE_HEIGHT//2) - ball['rect'].centery
-                # normalized_relative_intersection_y = intersect_y/(PADDLE_HEIGHT//2)
-                # bounce_angle = normalized_relative_intersection_y * MAX_BOUNCE_ANGLE
-                # ball['x_vel'] = round(BALL_START_VEL * math.cos(bounce_angle))
-                # ball['y_vel'] = round(BALL_START_VEL * math.sin(bounce_angle))
-                play_random_ball_hit_sound()
+                # TODO: collision physics
+                intersect_y = (paddle.centery + PADDLE_HEIGHT//2) - self.rect.centery
+                normalized_relative_intersection_y = intersect_y/(PADDLE_HEIGHT//2)
+                bounce_angle = normalized_relative_intersection_y * MAX_BOUNCE_ANGLE
+                self.x_vel = round(BALL_START_VEL * math.cos(bounce_angle))
+                self.y_vel = round(BALL_START_VEL * math.sin(bounce_angle))
+                self.play_random_ball_hit_sound()
 
         if self.rect.y < 0:
             self.y_vel *= -1
@@ -130,10 +173,10 @@ class Ball:
             self.y_vel *= -1
 
         if self.rect.x < 0:
-            pygame.event.post(pygame.event.Event(GameEvents.P2_SCORED))
+            pygame.event.post(pygame.event.Event(P2_SCORED))
 
         if self.rect.x > WINDOW_WIDTH:
-            pygame.event.post(pygame.event.Event(GameEvents.P2_SCORED))
+            pygame.event.post(pygame.event.Event(P1_SCORED))
     
     def play_random_ball_hit_sound(self):
         if random.randint(0,1) == 1:
@@ -141,29 +184,18 @@ class Ball:
         else:
             BALL_HIT_SOUND_2.play()
 
+    def draw(self):
+        pygame.draw.rect(self.window, WHITE, self.rect)
 
-BALL_HIT_SOUND_1.set_volume(VOLUME)
-BALL_HIT_SOUND_2.set_volume(VOLUME)
-SCORE_SOUND.set_volume(VOLUME)
+## Border
+class Border:
+    """The border that is in the middle of the pong playfield"""
+    def __init__(self, window):
+        self.rect = pygame.Rect(WINDOW_WIDTH//2 - 5, 0, 10, WINDOW_HEIGHT)
+        self.window = window
 
-# TODO: Make a dotted line border
-BORDER = pygame.Rect(WINDOW_WIDTH//2 - 5, 0, 10, WINDOW_HEIGHT)
-
-
-def draw_window(player1, player2, ball, player1_score, player2_score):
-    WIN.fill(BLACK)
-    pygame.draw.rect(WIN, WHITE, BORDER)
-
-    pygame.draw.rect(WIN, WHITE, player1)
-    pygame.draw.rect(WIN, WHITE, player2)
-    
-    p1_score = SCORE_FONT.render(str(player1_score), 1, WHITE)
-    p2_score = SCORE_FONT.render(str(player2_score), 1, WHITE)
-    WIN.blit(p1_score, (WINDOW_WIDTH//4 - p1_score.get_width()//2, 10))
-    WIN.blit(p2_score, ((WINDOW_WIDTH//4)*3 - p2_score.get_width()//2, 10))
-    
-    pygame.draw.rect(WIN, WHITE, ball['rect'])
-    pygame.display.update()
+    def draw(self):
+        pygame.draw.rect(self.window, WHITE, self.rect)
 
 def draw_start_text(text):
     draw_text = START_FONT.render(text, 1, WHITE)
@@ -172,12 +204,20 @@ def draw_start_text(text):
     pygame.time.delay(500)
 
 def main():
-    player1 = Paddle(P1_STARTING_X, P1_STARTING_Y, PADDLE_WIDTH, PADDLE_HEIGHT, 1)
-    player2 = Paddle(P2_STARTING_X, P2_STARTING_Y, PADDLE_WIDTH, PADDLE_HEIGHT, 2)
-    ball = Ball()
+    window = Window(WINDOW_WIDTH, WINDOW_HEIGHT, "Pong")
+    player1 = Paddle(window.get_surface(), P1_STARTING_X, P1_STARTING_Y, PADDLE_WIDTH, PADDLE_HEIGHT, 1)
+    player2 = Paddle(window.get_surface(), P2_STARTING_X, P2_STARTING_Y, PADDLE_WIDTH, PADDLE_HEIGHT, 2)
+    ball = Ball(window.get_surface())
+    border = Border(window.get_surface())
+    paddles = [player1.get_rect(), player2.get_rect()]
 
-    game_state = GameState.READY
-    start_text = "Press space to start a round"
+    window.add_draw_item(player1)
+    window.add_draw_item(player2)
+    window.add_draw_item(ball)
+    window.add_draw_item(border)
+    
+    window.set_game_state(GameStates.READY)
+    window.show_text("Press space to start round")
 
     clock = pygame.time.Clock()
     run = True
@@ -188,33 +228,33 @@ def main():
                 run = False
         
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE and game_state == GameState.READY:
+                if event.key == pygame.K_SPACE and window.get_game_state() == GameStates.READY:
                     # Hide message to press space to start
-                    start_text = ""
-                    game_state = GameState.PLAYING
-                    start_ball(ball)
+                    window.show_text("")
+                    window.set_game_state(GameStates.PLAYING)
+                    ball.spawn()
             
             if event.type == P1_SCORED:
-                player1_score += 1
+                player1.scored()
                 SCORE_SOUND.play()
+                window.show_text("Player 1 Scored!")
+                pygame.time.delay(SCORE_DELAY_TIME)
+                window.show_text("")
                 ball.spawn()
-                start_ball(ball)
             if event.type == P2_SCORED:
-                player2_score += 1
+                player2.scored()
                 SCORE_SOUND.play()
-                ball['rect'] = pygame.Rect(WINDOW_WIDTH//2, WINDOW_HEIGHT//2, 8,8)
-                start_ball(ball)
-
-        if start_text != "":
-            draw_start_text(start_text)
+                window.show_text("Player 2 Scored!")
+                pygame.time.delay(SCORE_DELAY_TIME)
+                window.show_text("")
+                ball.spawn()
 
         keys_pressed = pygame.key.get_pressed()
-        if game_state == GameState.PLAYING:
+        if window.get_game_state() == GameStates.PLAYING:
             player1.handle_movement(keys_pressed)
             player2.handle_movement(keys_pressed)
-            paddles = [player1.get_rect(), player2.get_rect()]
             ball.handle_movement(paddles)
-            draw_window(player1, player2, ball, player1_score, player2_score) ## TODO
+            window.draw()
 
     pygame.quit()
 
