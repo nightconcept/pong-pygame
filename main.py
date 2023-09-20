150044b7f7ac2a92f1631aade0b4da7fbddd9736
Danny Solivan <dark@nightconcept.net>
2023-09-19 21:06:42 -0700

    More changes to classes

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 import pygame
 import random
 
-
 # Inits required first
 pygame.init()
 pygame.font.init()
@@ -14,6 +13,12 @@ pygame.mixer.init()
 ## Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+
+## Controls
+CONTROL_P1_UP = pygame.K_w
+CONTROL_P1_DOWN = pygame.K_s
+CONTROL_P2_UP = pygame.K_UP
+CONTROL_P2_DOWN = pygame.K_DOWN
 
 ## Events
 class GameEvents(Enum):
@@ -26,15 +31,21 @@ START_FONT = pygame.font.Font(os.path.join('assets', 'bit5x3.ttf'), 20)
 
 ## Game Specific
 PLAYER_VEL = 30
+BALL_RADIUS = 5
 BALL_START_VEL = 6
 PADDLE_HEIGHT = 60
 PADDLE_WIDTH = 15
 MAX_BOUNCE_ANGLE = 45
 PLAYER_AREA_MARGIN = 10
+P1_STARTING_X = 100
+P1_STARTING_Y = 300
+P2_STARTING_X = 700
+P2_STARTING_Y = 300
 
 ## General Settings
 FPS = 60
-WINDOW_WIDTH, WINDOW_HEIGHT = 858, 525
+VOLUME = 0.3
+WINDOW_WIDTH, WINDOW_HEIGHT = 858, 525 # Based on actual pong resolution
 
 ## Sounds
 BALL_HIT_SOUND_1 = pygame.mixer.Sound(os.path.join('assets', 'ping_pong_8bit_plop.ogg'))
@@ -49,42 +60,88 @@ class GameStates(Enum):
 # Classes
 ## Window
 class Window:
-    def __init__(width, height, caption):
+    def __init__(self, width, height, caption):
         self.win = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption(caption)
 
     def draw():
         self.win.fill(BLACK)
-
         pygame.display.update()
+
+
 ## Paddle
 class Paddle:
-    def __init__(x, y, width, height, player):
+    def __init__(self, x, y, width, height, player):
         self.rect = pygame.Rect(x, y, width, height)
         self.player = player
+        self.score = 0
     
-    def handle_movement():
+    def handle_movement(self, keys_pressed):
         if self.player == 1:
-            if keys_pressed[pygame.K_w] and self.rect.y - PLAYER_VEL > PLAYER_AREA_MARGIN: # UP
+            if keys_pressed[CONTROL_P1_UP] and self.rect.y - PLAYER_VEL > PLAYER_AREA_MARGIN: # UP
                 player1.y -= PLAYER_VEL
-            if keys_pressed[pygame.K_s] and selft.rect.y + PLAYER_VEL + self.rect.height < WINDOW_HEIGHT - PLAYER_AREA_MARGIN: # DOWN
+            if keys_pressed[CONTROL_P1_DOWN] and selft.rect.y + PLAYER_VEL + self.rect.height < WINDOW_HEIGHT - PLAYER_AREA_MARGIN: # DOWN
                 player1.y += PLAYER_VEL
         else:
-            if keys_pressed[pygame.K_UP] and self.rect.y - PLAYER_VEL > 10: # UP
+            if keys_pressed[CONTROL_P2_UP] and self.rect.y - PLAYER_VEL > PLAYER_AREA_MARGIN: # UP
                 self.rect.y -= PLAYER_VEL
-            if keys_pressed[pygame.K_DOWN] and self.rect.y + PLAYER_VEL + self.rect.height < WINDOW_HEIGHT - 10: # DOWN
+            if keys_pressed[CONTROL_P2_DOWN] and self.rect.y + PLAYER_VEL + self.rect.height < WINDOW_HEIGHT - PLAYER_AREA_MARGIN: # DOWN
                 self.rect.y += PLAYER_VEL
+    
+    def get_rect():
+        return self.rect
 
 ## Ball
 class Ball:
     """The ball that is hit by the paddles in pong"""
-    def __init__(x, y):
-        self.x = x
-        self.y = y``
+    def __init__(self):
+        self.rect = pygame.draw.circle(WINDOW, WHITE, (-2 * BALL_RADIUS, -2 * BALL_RADIUS), BALL_RADIUS)
 
-BALL_HIT_SOUND_1.set_volume(0.2)
-BALL_HIT_SOUND_2.set_volume(0.2)
-SCORE_SOUND.set_volume(0.2)
+    def spawn(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+        self.x_vel = random.randint(BALL_START_VEL//2, BALL_START_VEL)
+        if random.randint(0, 1) == 1:
+            self.x_vel *= -1
+        self.y_vel = random.randint(BALL_START_VEL//2, BALL_START_VEL)
+        if random.randint(0, 1) == 1:
+            self.y_vel *= -1
+
+    def handle_movement(self, paddles):
+        self.rect.x += self.x_vel
+        self.rect.y += self.y_vel
+        for paddle in paddles:
+            if self.rect.colliderect(paddle):
+                # collision physics
+                # intersect_y = (player1.centery + PADDLE_HEIGHT//2) - ball['rect'].centery
+                # normalized_relative_intersection_y = intersect_y/(PADDLE_HEIGHT//2)
+                # bounce_angle = normalized_relative_intersection_y * MAX_BOUNCE_ANGLE
+                # ball['x_vel'] = round(BALL_START_VEL * math.cos(bounce_angle))
+                # ball['y_vel'] = round(BALL_START_VEL * math.sin(bounce_angle))
+                play_random_ball_hit_sound()
+
+        if self.rect.y < 0:
+            self.y_vel *= -1
+
+        if self.rect.y > WINDOW_HEIGHT:
+            self.y_vel *= -1
+
+        if self.rect.x < 0:
+            pygame.event.post(pygame.event.Event(GameEvents.P2_SCORED))
+
+        if self.rect.x > WINDOW_WIDTH:
+            pygame.event.post(pygame.event.Event(GameEvents.P2_SCORED))
+    
+    def play_random_ball_hit_sound(self):
+        if random.randint(0,1) == 1:
+            BALL_HIT_SOUND_1.play()
+        else:
+            BALL_HIT_SOUND_2.play()
+
+
+BALL_HIT_SOUND_1.set_volume(VOLUME)
+BALL_HIT_SOUND_2.set_volume(VOLUME)
+SCORE_SOUND.set_volume(VOLUME)
 
 # TODO: Make a dotted line border
 BORDER = pygame.Rect(WINDOW_WIDTH//2 - 5, 0, 10, WINDOW_HEIGHT)
@@ -111,74 +168,10 @@ def draw_start_text(text):
     pygame.display.update()
     pygame.time.delay(500)
 
-def handle_player1_movement(keys_pressed, player1):
-    if keys_pressed[pygame.K_w] and player1.y - PLAYER_VEL > 10: # UP
-        player1.y -= PLAYER_VEL
-    if keys_pressed[pygame.K_s] and player1.y + PLAYER_VEL + player1.height < WINDOW_HEIGHT - 10: # DOWN
-        player1.y += PLAYER_VEL
-
-def handle_player2_movement(keys_pressed, player2):
-    if keys_pressed[pygame.K_UP] and player2.y - PLAYER_VEL > 10: # UP
-        player2.y -= PLAYER_VEL
-    if keys_pressed[pygame.K_DOWN] and player2.y + PLAYER_VEL + player2.height < WINDOW_HEIGHT - 10: # DOWN
-        player2.y += PLAYER_VEL
-
-def handle_ball(ball, player1, player2):
-    ball['rect'].x += ball['x_vel']
-    ball['rect'].y += ball['y_vel']
-
-    if player1.colliderect(ball['rect']):
-        intersect_y = (player1.centery + PADDLE_HEIGHT//2) - ball['rect'].centery
-        normalized_relative_intersection_y = intersect_y/(PADDLE_HEIGHT//2)
-        bounce_angle = normalized_relative_intersection_y * MAX_BOUNCE_ANGLE
-        ball['x_vel'] = round(BALL_START_VEL * math.cos(bounce_angle))
-        ball['y_vel'] = round(BALL_START_VEL * math.sin(bounce_angle))
-        play_random_hit_sound()
-    if player2.colliderect(ball['rect']):
-        intersect_y = (player2.centery + PADDLE_HEIGHT//2) - ball['rect'].centery
-        normalized_relative_intersection_y = intersect_y/(PADDLE_HEIGHT//2)
-        bounce_angle = normalized_relative_intersection_y * MAX_BOUNCE_ANGLE
-        ball['x_vel'] = -1 * round(BALL_START_VEL * math.cos(bounce_angle))
-        ball['y_vel'] = -1 * round(BALL_START_VEL * math.sin(bounce_angle))
-        play_random_hit_sound()
-
-    if ball['rect'].y < 0:
-        ball['y_vel'] *= -1
-
-    if ball['rect'].y > WINDOW_HEIGHT:
-        ball['y_vel'] *= -1
-
-    if ball['rect'].x < 0:
-        pygame.event.post(pygame.event.Event(P2_SCORED))
-
-    if ball['rect'].x > WINDOW_WIDTH:
-        pygame.event.post(pygame.event.Event(P1_SCORED))
-
-def play_random_hit_sound():
-    if random.randint(0,1) == 1:
-        BALL_HIT_SOUND_1.play()
-    else:
-        BALL_HIT_SOUND_2.play()
-
-def start_ball(ball):
-    ball['x_vel'] = random.randint(BALL_START_VEL//2, BALL_START_VEL)
-    if random.randint(0, 1) == 1:
-        ball['x_vel'] *= -1
-    ball['y_vel'] = random.randint(BALL_START_VEL//2, BALL_START_VEL)
-    if random.randint(0, 1) == 1:
-        ball['y_vel'] *= -1
-
 def main():
-    player1 = pygame.Rect(100, 300, PADDLE_WIDTH, PADDLE_HEIGHT)
-    player2 = pygame.Rect(700, 300, PADDLE_WIDTH, PADDLE_HEIGHT)
-    ball = {
-        "x_vel": 0,
-        "y_vel": 0,
-        "rect": pygame.Rect(WINDOW_WIDTH//2, WINDOW_HEIGHT//2, 8,8)
-    }
-    
-    player1_score = 0
-    player2_score = 0
+    player1 = Paddle(P1_STARTING_X, P1_STARTING_Y, PADDLE_WIDTH, PADDLE_HEIGHT, 1)
+    player2 = Paddle(P2_STARTING_X, P2_STARTING_Y, PADDLE_WIDTH, PADDLE_HEIGHT, 2)
+    ball = Ball()
 
     game_state = GameState.READY
     start_text = "Press space to start a round"
@@ -201,7 +194,7 @@ def main():
             if event.type == P1_SCORED:
                 player1_score += 1
                 SCORE_SOUND.play()
-                ball['rect'] = pygame.Rect(WINDOW_WIDTH//2, WINDOW_HEIGHT//2, 8,8)
+                ball.spawn()
                 start_ball(ball)
             if event.type == P2_SCORED:
                 player2_score += 1
@@ -214,10 +207,11 @@ def main():
 
         keys_pressed = pygame.key.get_pressed()
         if game_state == GameState.PLAYING:
-            handle_player1_movement(keys_pressed, player1)
-            handle_player2_movement(keys_pressed, player2)
-            handle_ball(ball, player1, player2)
-            draw_window(player1, player2, ball, player1_score, player2_score)
+            player1.handle_movement(keys_pressed)
+            player2.handle_movement(keys_pressed)
+            paddles = [player1.get_rect(), player2.get_rect()]
+            ball.handle_movement(paddles)
+            draw_window(player1, player2, ball, player1_score, player2_score) ## TODO
 
     pygame.quit()
 
